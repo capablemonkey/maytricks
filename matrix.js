d41dadea166118c8857dc063ff0528f14e580168
Gordon Zheng <technix1@gmail.com>
2014-04-19 13:54:26 -0400

    refactor JS

--- matrix.js
+++ matrix.js
@@ -1,19 +1,19 @@
-begin = 33;
-end = 126;
-size = 500;
-cut = 150;
-height = 35;
-delay = 200;
+ASCII_BEGIN = 33;
+ASCII_END = 126;
+TEXT_LENGTH = 500;
+CHARS_PER_ROW = 150;
+REFRESH_FREQUENCY = 200;
 
-function refresh(data) {
-	for (var x=0; x < data.length; x++) {
-		data[x] = String.fromCharCode(Math.floor((Math.random() * (end - begin)) + begin))
+function initMatrix(asciiBegin, asciiEnd, textLength, charsPerRow, refreshInterval) {
+	// regenerates content of the matrix
+	function refresh(data) {
+		for (var x=0; x < data.length; x++) {
+			data[x] = String.fromCharCode(Math.floor((Math.random() * (asciiEnd - asciiBegin)) + asciiBegin))
+		}
+		return data.join("");
 	}
-	return data.join("");
-}
 
-function init() {
-	// determine window height, width:
+	// determine how many lines we need to fill window
 	h = $(window).height();
 	lineheight = $(".line").first().height()
 	lines = Math.ceil(h / lineheight)
@@ -22,17 +22,28 @@ function init() {
 		$("#lines").append($(".line").first().clone())
 	}
 
-	data = Array(size);
-	data.length = size;
+	// array to contain matrix content
+	data = Array(textLength);
+	data.length = textLength;
 
 	window.setInterval(function() {
 		p = refresh(data);
+		// give each line a random slice of the pie
 		$(".line").each(function(index, ele) {
-			i = Math.floor(Math.random()* (size - cut))
-			$(ele).text(p.slice(i, i + cut))
+			i = Math.floor(Math.random()* (textLength - charsPerRow))
+			$(ele).text(p.slice(i, i + charsPerRow))
 		})
-	}, delay);
+	}, refreshInterval);
 }
 
-$(document).ready(init);
+function initConsole() {
+	// TODO
+}
+
+function initAll() {
+	initMatrix(ASCII_BEGIN, ASCII_END, TEXT_LENGTH, CHARS_PER_ROW, REFRESH_FREQUENCY);
+	initConsole();
+}
+
+$(document).ready(initAll);
 
